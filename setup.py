0c72267172640e1dd6de48c59c5ff4e0f2a77d28
Vedant Chandra <me@vedantchandra.com>
2021-08-11 18:10:17 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,5 +11,5 @@ setup(name='corv',
       packages=['corv'],
       package_data={'corv':['pkl/*']},
       dependency_links = [],
-      install_requires=['scipy', 'lmfit'],
+      install_requires=['numpy=1.22', 'scipy', 'lmfit'],
       include_package_data=True)
