44afc134f9059ab59a0be4db3814febc5f0b8cc4
Vedant Chandra <me@vedantchandra.com>
2021-08-16 13:11:14 -0400

    unbreak tests

--- eval_rvs.py
+++ eval_rvs.py
@@ -11,6 +11,24 @@ RV fitting scheme.
 """
 
 import corv
+import numpy as np
+
+
+wl = np.linspace(4000, 8000, 8000)
+corvmodel = corv.models.make_koester_model()
+params = corvmodel.make_params()
+params['RV'].set(value = 132)
+fl = corvmodel.eval(params, x = wl)
+sig = 0.1 * np.median(fl)
+fl += sig * np.random.normal(size = len(wl))
+ivar = 0*fl + 1 / (sig)**2
+
+
+param_res, rv_res, rv_init = corv.fit.fit_corv(wl, fl, ivar, corvmodel)
+
+print(rv_init)
+print(param_res.params['RV'])
+print(rv_res.params['RV'])
 
 # RV and RV error vs S/N
 
--- test_corv.py
+++ test_corv.py
@@ -11,20 +11,20 @@ import numpy as np
 
 import corv
 
-# def test_travis():
-#     wl = np.linspace(4000, 8000, 8000)
+def test_travis():
+    wl = np.linspace(4000, 8000, 8000)
 
-#     plt.figure(figsize = (7, 7))
-#     corvmodel = corv.models.make_balmer_model(names = ['a'])
-#     params = corvmodel.make_params()
-#     nwl, nfl = corv.models.get_normalized_model(wl, corvmodel, params)
-#     plt.plot(nwl, nfl, 'k.')
-#     params['RV'].set(value = 1000)
-#     nwl, nfl = corv.models.get_normalized_model(wl, corvmodel, params)
-#     plt.plot(nwl, nfl, 'r.')
-#     plt.title('Voigt Balmer')
+    plt.figure(figsize = (7, 7))
+    corvmodel = corv.models.make_balmer_model(names = ['a'])
+    params = corvmodel.make_params()
+    nwl, nfl = corv.models.get_normalized_model(wl, corvmodel, params)
+    plt.plot(nwl, nfl, 'k.')
+    params['RV'].set(value = 1000)
+    nwl, nfl = corv.models.get_normalized_model(wl, corvmodel, params)
+    plt.plot(nwl, nfl, 'r.')
+    plt.title('Voigt Balmer')
 
-# test_travis()
+test_travis()
 
 
 # wl = np.linspace(4000, 8000, 8000)
@@ -38,19 +38,3 @@ import corv
 # nwl, nfl = corv.models.get_normalized_model(wl, corvmodel, params)
 # plt.plot(nwl, nfl, 'r.')
 # plt.title('Koester DA')
-
-wl = np.linspace(4000, 8000, 8000)
-corvmodel = corv.models.make_koester_model()
-params = corvmodel.make_params()
-params['RV'].set(value = 132)
-fl = corvmodel.eval(params, x = wl)
-sig = 0.1 * np.median(fl)
-fl += sig * np.random.normal(size = len(wl))
-ivar = 0*fl + 1 / (sig)**2
-
-
-param_res, rv_res, rv_init = corv.fit.fit_corv(wl, fl, ivar, corvmodel)
-
-print(rv_init)
-print(param_res.params['RV'])
-print(rv_res.params['RV'])
