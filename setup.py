98fe4e4d4e06d88f924ae4bc8ae20a75c1e8baa5
Vedant Chandra <me@vedantchandra.com>
2022-01-26 14:35:17 +0530

    add tqdm as dependancy

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup,find_packages
 
 setup(name='corv',
-      version='0.1',
-      description='compact object radial velocities',
-      author='Vedant Chandra',
-      author_email='vedant.chandra@cfa.harvard.edu',
-      license='MIT',
-      url='https://github.com/vedantchandra/corv',
-      package_dir = {"" : "src"},
-      packages=find_packages(where='src'),
-      package_data={'corv':['models/*']},
-      dependency_links = [],
-      install_requires=['numpy', 'scipy', 'lmfit', 'matplotlib', 'astropy'],
-      include_package_data=True)
+	version='0.1',
+	description='compact object radial velocities',
+	author='Vedant Chandra',
+	author_email='vedant.chandra@cfa.harvard.edu',
+	license='MIT',
+	url='https://github.com/vedantchandra/corv',
+	package_dir = {"" : "src"},
+	packages=find_packages(where='src'),
+	package_data={'corv':['models/*']},
+	dependency_links = [],
+	install_requires=['numpy', 'scipy', 'lmfit', 'matplotlib', 'astropy', 'tqdm'],
+	include_package_data=True)
