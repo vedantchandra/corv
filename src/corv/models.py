82d3a2f8919294316ea26476826449f837a0f4de
Stefan Arseneau <arseneausm@gmail.com>
2025-05-05 19:12:28 -0400

    update

--- src/corv/models.py
+++ src/corv/models.py
@@ -224,7 +224,12 @@ class WarwickDAModel:
             self.model.set_param_hint('teff', min = 4001, max = 39900, value = 12000)
         else:
             self.model.set_param_hint('teff', min = 4001, max = 129000, value = 12000)
-        self.model.set_param_hint('logg', min = 7, max = 9.49, value = 8)
+
+        if model_name == '1d_da_nlte':
+            self.model.set_param_hint('logg', min = 7, max = 9.49, value = 8)
+        else:
+            self.model.set_param_hint('logg', min = 7, max = 9, value = 8)
+            
         self.model.set_param_hint('RV', min = -2500, max = 2500, value = 0)
         self.model.set_param_hint('res', value = resolution, min = 0, vary = False)
     
